ae825e2f6b5457888bb61eb50175697dc8b3d666
shlomihaimov1 <shlomihaimov1@gmail.com>
2025-03-15 22:51:33 +0200

    add upload post capability

--- front/src/services/postService.tsx
+++ front/src/services/postService.tsx
@@ -1,6 +1,27 @@
+import axios from "axios";
+import { BACKEND_URL } from "../globalVariables";
+import { getTokens } from "./globalService";
 
 
-
-export const fetchPosts = async () => {
-    // Get posts logic here
+export const createPost = async (postData: FormData) => {
+    const { accessToken, refreshToken } = getTokens();
+    try {
+        const response = await axios.post(
+            `${BACKEND_URL}/posts`,
+            postData,
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${accessToken}`,
+                    "x-refresh-token": refreshToken,
+                },
+            }
+        );
+        
+        return response.data;
+    } catch (error) {
+        console.error("Error creating post:", error);
+        throw error;
+    }
 };
+
